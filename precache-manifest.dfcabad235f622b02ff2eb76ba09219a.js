d91baf70fe9e02553c93f5b8485e538f8c038ea7
Suzy <suzy.nakayama@gmail.com>
2025-08-14 10:58:14 -0400

    Updates

---
+++ precache-manifest.dfcabad235f622b02ff2eb76ba09219a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5693f7f1158e85501eccf5b2553fac6b",
+    "url": "/Suzy-Nakayama-v1/index.html"
+  },
+  {
+    "revision": "4a575f8ffa7c3aa86c84",
+    "url": "/Suzy-Nakayama-v1/static/css/main.19863df3.chunk.css"
+  },
+  {
+    "revision": "ffb1e82fd31011574580",
+    "url": "/Suzy-Nakayama-v1/static/js/2.62a80942.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/Suzy-Nakayama-v1/static/js/2.62a80942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a575f8ffa7c3aa86c84",
+    "url": "/Suzy-Nakayama-v1/static/js/main.545cb8b1.chunk.js"
+  },
+  {
+    "revision": "63f121503203d6b19d0e",
+    "url": "/Suzy-Nakayama-v1/static/js/runtime-main.879c5e5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Suzy-Nakayama/precache-manifest.7b63260e30d7349039c0c28267074463.js"
+  "/Suzy-Nakayama-v1/precache-manifest.dfcabad235f622b02ff2eb76ba09219a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Suzy-Nakayama/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Suzy-Nakayama-v1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
