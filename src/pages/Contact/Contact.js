15bad49bc3fc43fc10da7d1518dc02562382336c
Suzy <suzy.nakayama@gmail.com>
2025-08-14 22:07:45 -0400

    disabling emailjs

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,106 +1,108 @@
-import React, { useState } from "react";
-import emailjs from "emailjs-com";
-import TrackVisibility from "react-on-screen";
-import "./Contact.css";
+import React, { useState } from 'react';
+// import emailjs from 'emailjs-com';
+import TrackVisibility from 'react-on-screen';
+import './Contact.css';
 import { Panda } from '../../components/Panda/Panda';
 
 const Contact = () => {
-	const [message, setMessage] = useState({
-		name: "",
-		email: "",
-		msg: "",
+  const [message, setMessage] = useState({
+    name: '',
+    email: '',
+    msg: '',
   });
 
-	let resetForm = () => {
-		setMessage({
-			name: "",
-			email: "",
-			msg: "",
-		});
-	};
+  let resetForm = () => {
+    setMessage({
+      name: '',
+      email: '',
+      msg: '',
+    });
+  };
 
-	let handleChange = (evt) => {
-		setMessage({
-			...message,
-			[evt.target.name]: [evt.target.value],
-		});
-	};
+  let handleChange = evt => {
+    setMessage({
+      ...message,
+      [evt.target.name]: [evt.target.value],
+    });
+  };
 
-	let handleSubmit = (evt) => {
-		evt.preventDefault();
-		let templateParams = {
-			user_name: message.name,
-			user_email: message.email,
-			to_name: process.env.REACT_APP_EMAILJS_RECEIVER,
-			message_html:
-				message.msg + `   From: ${message.name}     Email: ${message.email}`,
-		};
-		emailjs.send(
-			"gmail",
-			process.env.REACT_APP_EMAILJS_TEMPLATEID,
-			templateParams,
-			process.env.REACT_APP_EMAILJS_USERID
-		);
-		resetForm();
-	};
+  let handleSubmit = evt => {
+    evt.preventDefault();
+    //! Commented out emailjs code for now, as it is not being used
+    // let templateParams = {
+    //   user_name: message.name,
+    //   user_email: message.email,
+    //   to_name: process.env.REACT_APP_EMAILJS_RECEIVER,
+    //   message_html:
+    //     message.msg + `   From: ${message.name}     Email: ${message.email}`,
+    // };
+    // emailjs.send(
+    //   'gmail',
+    //   process.env.REACT_APP_EMAILJS_TEMPLATEID,
+    //   templateParams,
+    //   process.env.REACT_APP_EMAILJS_USERID
+    // );
+    resetForm();
+  };
 
-	return (
-    <div className="contact" id="contact">
+  return (
+    <div className='contact' id='contact'>
       <TrackVisibility>
-        <Panda className="panda" />
+        <Panda className='panda' />
       </TrackVisibility>
-      <form className="contact__form" onSubmit={handleSubmit}>
-        <h2 className="contact__form--title">Let's chat!</h2>
-        <p className="contact__form--text">
+      <form className='contact__form' onSubmit={handleSubmit}>
+        <h2 className='contact__form--title'>Let's chat!</h2>
+        <p className='contact__form--text'>
           Fill out below and I'll do my best to get back to you as soon as I
           can! &nbsp;&nbsp;&nbsp;
-          <i className="far fa-smile-wink"></i>
+          <i className='far fa-smile-wink'></i>
         </p>
-        <div className="contact__form--form">
-          <label className="contact__label--name" htmlFor="name">
+        <div className='contact__form--form'>
+          <label className='contact__label--name' htmlFor='name'>
             Name:
           </label>
           <input
-            className="contact__input--name"
-            name="name"
-            id="name"
-            type="text"
+            className='contact__input--name'
+            name='name'
+            id='name'
+            type='text'
             value={message.name}
             onChange={handleChange}
           ></input>
-          <label className="contact__label--email" htmlFor="email">
+          <label className='contact__label--email' htmlFor='email'>
             Email:
           </label>
           <input
-            className="contact__input--email"
-            name="email"
-            id="email"
-            type="email"
+            className='contact__input--email'
+            name='email'
+            id='email'
+            type='email'
             value={message.email}
             onChange={handleChange}
           ></input>
-          <label className="contact__label--msg" htmlFor="msg">
+          <label className='contact__label--msg' htmlFor='msg'>
             Message
           </label>
           <textarea
-            className="contact__input--msg"
-            name="msg"
-            id="msg"
-            rows="10"
-            cols="50"
+            className='contact__input--msg'
+            name='msg'
+            id='msg'
+            rows='10'
+            cols='50'
             value={message.msg}
             onChange={handleChange}
           ></textarea>
         </div>
         <input
-          type="submit"
-          className="contact__form--submit"
-          value="Send"
+          type='submit'
+          className='contact__form--submit'
+          value='Send'
           onSubmit={handleSubmit}
+          disabled={true}
         />
       </form>
     </div>
   );
-}
+};
 
 export default Contact;
